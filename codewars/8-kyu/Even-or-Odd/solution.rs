226eb233060f176348543876cdef0e463db32242
xen <selegakamil@gmail.com>
2022-11-28 23:04:10 +0100

    Add the instruction for the problem

--- codewars/8-kyu/Even-or-Odd/solution.rs
+++ codewars/8-kyu/Even-or-Odd/solution.rs
@@ -1,3 +1,7 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. 
+*/
+
 fn even_or_odd(i: i32) -> &'static str {
     if i % 2 == 0 { "Even" } else { "Odd" }
 }
