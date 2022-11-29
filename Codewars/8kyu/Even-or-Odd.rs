999856d9b19b11d1ba7e1f338b695417144eba62
xen <selegakamil@gmail.com>
2022-11-29 19:44:01 +0100

    Move the solutions to parent difficulty directory, add Reversed-Strings kata

--- Codewars/8kyu/Even-or-Odd.rs
+++ Codewars/8kyu/Even-or-Odd.rs
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 /*
-Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. 
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
 fn even_or_odd(i: i32) -> &'static str {
